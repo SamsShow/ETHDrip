0cbeffd896ee12c39a79a929b0e6c07e88fee5e6
SamsShow <sakshamtyagi2008@gmail.com>
2025-09-30 17:25:36 +0530

    Initial Commit

---
+++ ETHDrip/ETHDripApp.swift
@@ -0,0 +1,17 @@
+//
+//  ETHDripApp.swift
+//  ETHDrip
+//
+//  Created by Saksham Tyagi on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ETHDripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ETHDripTests/ETHDripTests.swift
@@ -0,0 +1,17 @@
+//
+//  ETHDripTests.swift
+//  ETHDripTests
+//
+//  Created by Saksham Tyagi on 30/09/25.
+//
+
+import Testing
+@testable import ETHDrip
+
+struct ETHDripTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
